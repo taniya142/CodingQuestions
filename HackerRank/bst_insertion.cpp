c97a8a54fc344db25db39273c66a8ba6f2678e57
taniya142 <taniyagehlaut14@gmail.com>
2021-03-19 22:25:03 +0530

    insert

---
+++ HackerRank/bst_insertion.cpp
@@ -0,0 +1,18 @@
+Node * insert(Node * root, int data) {
+        
+        if(root==nullptr){
+            return new Node(data);
+        }
+        else{
+            Node* cur; 
+            if(data<=root->data){
+                cur=insert(root->left, data);
+                root->left=cur;
+            }else{
+                cur=insert(root->right, data);
+                root->right=cur;
+            }
+        }
+
+        return root;
+    }

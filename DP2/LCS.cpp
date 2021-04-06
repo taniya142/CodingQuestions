bc1b73cc71eed6555cde3335403742dc3b3da820
Taniya <taniyagehlaut14@gmail.com>
2021-04-06 17:18:33 +0530

    lcs

---
+++ DP2/LCS.cpp
@@ -0,0 +1,32 @@
+int lcs(int x, int y, string s1, string s2){
+        
+        // your code here
+        if(x==0 || y==0){
+            return 0;
+        }
+        
+        else if(s1[x-1]==s2[y-1]){
+            return 1+lcs(x-1,y-1,s1,s2);
+        }else{
+            return max(lcs(x-1,y,s1,s2),lcs(x,y-1,s1,s2));
+        }
+        
+    }
+
+
+    // RECURSION
+
+  //     int lcs(int x, int y, string s1, string s2){
+        
+    //     // your code here
+    //     if(x==0 || y==0){
+    //         return 0;
+    //     }
+        
+    //     else if(s1[x-1]==s2[y-1]){
+    //         return 1+lcs(x-1,y-1,s1,s2);
+    //     }else{
+    //         return max(lcs(x-1,y,s1,s2),lcs(x,y-1,s1,s2));
+    //     }
+        
+    // }

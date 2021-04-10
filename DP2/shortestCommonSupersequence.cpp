8793108f544170ac1f4d959facda61e7b4db5481
Taniya <taniyagehlaut14@gmail.com>
2021-04-10 12:23:37 +0530

    shortestCommonSupersequence.cpp

---
+++ DP2/shortestCommonSupersequence.cpp
@@ -0,0 +1,31 @@
+int lcs(int x, int y, string s1, string s2){
+        
+        // your code here
+        int dp[x+1][y+1];
+        
+        for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                if(i==0  || j==0){
+                    dp[i][j]=0;
+                }
+                else if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j] ,dp[i][j-1]);
+                }
+            }
+        }
+        return dp[x][y];
+        
+    }
+int shortestCommonSupersequence(char* s1, char* s2, int x, int y) {
+    //code here
+    int longest=lcs(x,y,s1,s2);
+    
+    int ans=longest + (x-longest) + (y-longest);
+    
+    return ans;
+        
+    
+}
